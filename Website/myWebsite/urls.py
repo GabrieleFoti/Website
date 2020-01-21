f9209f5dbb6ef8f698d254d0a236985aa6860308
GabrieleFoti <6abri3le@gmail.com>
2020-01-21 23:15:57 +0100

    Sixth commit

--- Website/myWebsite/urls.py
+++ Website/myWebsite/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('login/', views.login, name = 'login'),
+    path('page/', views.login, name = 'login'),
 ]
--- Website/myWebsite/views.py
+++ Website/myWebsite/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return render(request, "home.html")
 
